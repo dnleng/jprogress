270d2467a71fc3860267b11d1fab4f6e8f8f7ea0
Daniel de Leng <daniel.de.leng@liu.se>
2018-04-30 13:02:18 +0200

    First commit

---
+++ src/main/java/se/liu/ida/jprogress/Main.java
@@ -0,0 +1,10 @@
+package se.liu.ida.jprogress;
+
+/**
+ * Created by dnleng on 30/04/18.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
