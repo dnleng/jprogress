7170d1d44fd8865d5860f1f2b2423c12f9b12108
Daniel de Leng <daniel.de.leng@liu.se>
2018-05-11 12:26:17 +0200

    Script updates

--- data/run.sh
+++ data/run.sh
@@ -1,4 +1,17 @@
-for i in $(seq 75 150)
+#!/usr/bin/env bash
+#for i in $(seq 75 150)
+#do
+#   java -Xms50g -Xmx50g -jar jprogressor.jar "kr-online-1-$i.csv" "false" "1" "$i"
+#done
+
+
+for i in $(seq 1 10)
 do
-   java -Xms50g -Xmx50g -jar jprogressor.jar "kr-online-1-$i.csv" "false" "1" "$i"
-done 
+   for t in 1 5 inf
+   do
+      for n in 175 200 225 250 inf
+      do
+         java -Xms50g -Xmx50g -jar jprogressor.jar "kr-online-$t-$n-$i.csv" "false" "$t" "$n"
+      done
+   done
+done
